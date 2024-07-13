d0558da91b866e570b6acf5e61ca834b21f9aa89
coder7475 <robiulhossain7475@gmail.com>
2024-07-13 22:54:25 +0600

    feat: learn mutable variable

--- 1/variables/1.rs
+++ 1/variables/1.rs
@@ -1,3 +1,9 @@
+/*
+* Binding and Mutability
+? 1. A variable can be used only if it has been initialized
+*/
 fn main() {
-    println!("Hello World!")
+    let x:i32 = 5;
+    assert_eq!(x, 5);
+    println!("Success!")
 }
