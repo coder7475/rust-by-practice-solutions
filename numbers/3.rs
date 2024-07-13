98dde6a6e875f2117073227727b85b3d5264b86d
coder7475 <robiulhossain7475@gmail.com>
2024-07-13 22:13:00 +0600

    feat: exercise 3 of numbers

---
+++ numbers/3.rs
@@ -0,0 +1,10 @@
+fn main() {
+ let x = 5; // ? i32
+//  println!("{}", type_of(&x));
+ assert_eq!("i32".to_string(), type_of(&x));
+ println!("Success!");
+}
+
+fn type_of<T>(_: &T) -> String {
+    format!("{}", std::any::type_name::<T>())
+}
