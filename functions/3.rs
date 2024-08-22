06d79bbce07969daa311b40ac96a046689ea628e
coder7475 <robiulhossain7475@gmail.com>
2024-08-22 06:51:35 +0600

    feat: update function 3rd exercsie

--- functions/3.rs
+++ functions/3.rs
@@ -1,4 +1,5 @@
-fn main()  {
+// * First Solution
+/* fn main()  {
   never_return();
   // make it unreachable
   println!("Failed!");
@@ -6,4 +7,21 @@ fn main()  {
 
 fn never_return()  {
   panic!("Thread Panicked");
+} */
+
+// * Second Solution
+fn main() {
+  never_return();
+}
+
+use std::thread;
+use std::time;
+
+fn never_return() -> ! {
+  // implement this function, don't modify fn signatures
+  loop {
+      println!("I return nothing");
+      // sleeping for 1 second to avoid exhausting the cpu resource
+      thread::sleep(time::Duration::from_secs(1))
+  }
 }
