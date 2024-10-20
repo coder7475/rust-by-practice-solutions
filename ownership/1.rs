ff54a9e188fc5aa7087bc1f6af813a2e9ee96d06
coder7475 <robiulhossain7475@gmail.com>
2024-10-20 06:52:17 +0600

    feat: sol 1 for ownership 3

--- ownership/1.rs
+++ ownership/1.rs
@@ -1,5 +1,24 @@
 fn main(){
+    
+    /*
+    * Solution 1
     let x = String::from("Hello World");
     let y = x.clone();
+    */
+
+    /*
+    * Solution 2
+    let x = "hello world";
+    let y = x;
+    */
+
+    /*
+    * Solution 3
+    let x = String::from("Hello World");
+    let y = &x;
+    */
+    let x = &String::from("hello, world");
+    let y = x;
+
     println!("{}, {}", x, y);
 }
