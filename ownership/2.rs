dd1b4f3bf3f94ecc42619591863c47f0b857fecd
coder7475 <robiulhossain7475@gmail.com>
2024-10-21 07:43:27 +0600

    feat: ownership ex 3

--- ownership/2.rs
+++ ownership/2.rs
@@ -1,4 +1,3 @@
-// Don't modify code in main!
 fn main() {
     let s1 = String::from("Hello world");
     let s2 = take_ownership(s1);
@@ -6,7 +5,6 @@ fn main() {
     println!("{}", s2);
 }
 
-// Only modify the code below!
 fn take_ownership(s: String) -> String {
     println!("{}", s);
     s
