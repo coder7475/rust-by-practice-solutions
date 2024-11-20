3a7c145ea4aa79d30b8043801f092c11c1b8a2ca
coder7475 <robiulhossain7475@gmail.com>
2024-11-20 17:51:57 +0600

    string ex 8: sol 1

---
+++ strings/8.rs
@@ -0,0 +1,8 @@
+// Use two approaches to fix the error and without adding a new line
+fn main() {
+    // Sol 1
+    let s = "hello, world".to_string();
+    let s1: String = s;
+
+    println!("Success!");
+}
